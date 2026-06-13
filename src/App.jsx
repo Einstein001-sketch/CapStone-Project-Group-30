c1f361f213ff7c8dd7bb84843bdbcc6ba5dc2cf0
Emmanuel Peter <60232645+Einstein001-sketch@users.noreply.github.com>
2026-06-14 00:26:25 +0100

    Update App.jsx
    Footer not displaying but fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
-import React from 'react';
-import Header from './Header';
-import Hero from './Hero'
+import React from "react";
+import Header from "./Header";
+import Hero from "./Hero";
+import Videosec from "./Videosec";
+import Imagesec from "./Imagesec.jsx";
+import Table from "./Table.jsx";
+import ContactForm from "./ContactForm.jsx";
+import Footer from "./Footer.jsx";
 
 const App = () => {
   return (
     <>
-    <Header/>
-    <Hero/>
+      <Header />
+      <Hero />
+      <Videosec />
+      <Imagesec />
+      <Table />
+      <ContactForm />
+      <Footer />
     </>
-  )
-}
+  );
+};
 
 export default App;
