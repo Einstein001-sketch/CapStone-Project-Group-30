d69b773357c33313b109e8deac6a8adf6e536ddf
Emmanuel Peter <60232645+Einstein001-sketch@users.noreply.github.com>
2026-06-13 22:31:32 +0100

    Update Hero.jsx
    Added the linking syntax

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -10,8 +10,8 @@ const Hero = () => {
             facts, from size and mass to gravity and density, this page<br/>
             breaks down the solar system in a clear, data driven way.
         </p>
-        <button className='ex-btn'><a href="">Explore the Data</a></button>
-        <button className='cont-btn'><a href="">Contact Us</a></button>
+        <button className='ex-btn'><a href="#Table.jsx">Explore the Data</a></button>
+        <button className='cont-btn'><a href="#Form.jsx">Contact Us</a></button>
         </div>
         <div className="hero-img">
             <img src={hero} alt="" />
